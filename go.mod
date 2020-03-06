b6b46e9eaa509d72b823d224b99d3cf6743620dd
yongfu <rickie622@gmail.com>
2020-03-06 10:47:03 +0800

    调整默认值,必须要写域名

---
+++ go.mod
@@ -0,0 +1,5 @@
+module doit-ssl-checker
+
+go 1.14
+
+require github.com/urfave/cli v1.22.2
--- main.go
+++ main.go
@@ -12,16 +12,16 @@ func main() {
 
 	app := cli.NewApp()
 	app.Name = "doit-ssl-checker"
-	app.Usage = " 检测对应域名的ssl工具"
+	app.Usage = "检测对应域名的ssl工具"
 	app.Author = "yongfu"
 	app.Description = "检查主机的https证书配置"
-	app.ArgsUsage = "\n  ./doit-ssl-checker <-d domainname> [-i ip] [-p port] [-l]"
+	app.UsageText = "./doit-ssl-checker <-d domainname> [-i ip] [-p port] [-l]"
 
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name:  "domain, d",
-			Value: "www.liaoyongfu.com",
-			Usage: "域名",
+			Name:     "domain, d",
+			Usage:    "域名",
+			Required: true,
 		},
 		cli.StringFlag{
 			Name:  "ip, i",
@@ -58,7 +58,6 @@ func main() {
 		conn, err := tls.Dial("tcp", addr, &cnf)
 		if err != nil {
 			fmt.Printf("[error]: %v", err.Error())
-			//log.Fatalln(err.Error())
 		}
 		defer conn.Close()
 		Dprint(conn, domain, verbose)
@@ -81,17 +80,17 @@ func Dprint(conn *tls.Conn, domain string, verbose bool) {
 		fmt.Printf("NotAfter: %v\n", key.NotAfter.Format("2006-01-02 15:04:05"))
 	} else {
 		for i, v := range status.PeerCertificates {
-			fmt.Printf("证书链: %v\n", i)
+			fmt.Printf("PerrCertificates: %v\n", i)
 			fmt.Printf("Issuer.CommonName: %#v\n", v.Issuer.String())
 			fmt.Printf("Subject: %#v\n", v.Subject.String())
 			fmt.Printf("DNSNames: %v\n", v.DNSNames)
 			fmt.Printf("NotBefor: %v\n", v.NotBefore.Format("2006-01-02 15:04:05"))
-			fmt.Printf("NotAfter: %v\n", v.NotAfter.Format("2006-01-02 15:04:05"))
+			fmt.Printf("NotAfter: %v\n\n", v.NotAfter.Format("2006-01-02 15:04:05"))
 		}
 
 		for i, v := range status.VerifiedChains {
 			for j, k := range v {
-				fmt.Printf("\ni: %v, j: %#v\n", i, j)
+				fmt.Printf("VerifiedChains i: %v, j: %#v\n", i, j)
 				fmt.Printf("Issuer: %#v\n", k.Issuer.String())
 				fmt.Printf("Subject: %#v\n", k.Subject.String())
 				fmt.Printf("NotBefore: %#v\n", k.NotBefore.Format("2006-01-02 15:04:05"))
